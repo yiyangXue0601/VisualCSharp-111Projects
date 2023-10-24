2b0d01c2b6ef739cbbb252b1667d1ad4b1495634
Yiyang Xue <YIYXUE952@STUDENT.CITRUSCOLLEGE.EDU>
2023-10-23 20:36:17 -0700

    Add project files.

---
+++ evenFibNumPractice/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace evenFibNumPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserNum = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(719, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of even Fibonacci numbers to display:";
+            // 
+            // txtUserNum
+            // 
+            this.txtUserNum.Font = new System.Drawing.Font("SimSun", 12F);
+            this.txtUserNum.Location = new System.Drawing.Point(737, 12);
+            this.txtUserNum.Name = "txtUserNum";
+            this.txtUserNum.Size = new System.Drawing.Size(249, 44);
+            this.txtUserNum.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btnSubmit.Location = new System.Drawing.Point(12, 88);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(974, 95);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("SimSun", 12F);
+            this.lblResult.Location = new System.Drawing.Point(12, 209);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(974, 212);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 443);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtUserNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Display Even Fibonacci Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserNum;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ evenFibNumPractice/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evenFibNumPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //declaration
+            //get user input
+            int.TryParse(txtUserNum.Text, out int totalNum);
+            int counter;
+            int firstNum = 0;
+            int secondNum = 1;
+            int thirdNum;
+            string output = "";
+
+            //calculation
+            counter = 0;
+            while (counter < totalNum)
+            {
+                if (firstNum % 2 == 0)
+                {
+                    output = output + " " + Convert.ToString(firstNum);
+                    counter++;
+                }
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+
+            }
+
+            //output
+            lblResult.Text = output;
+
+
+        }
+    }
+}
