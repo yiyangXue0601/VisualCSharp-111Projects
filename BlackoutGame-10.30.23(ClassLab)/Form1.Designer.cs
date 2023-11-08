22fe375c0faeef17c2570679439764f6780c672a
Yiyang Xue <YIYXUE952@STUDENT.CITRUSCOLLEGE.EDU>
2023-11-08 12:29:19 -0800

    Added BlackoutGame

---
+++ BlackoutGame-10.30.23(ClassLab)/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BlackoutGame_10._30._23_ClassLab_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn1.Location = new System.Drawing.Point(99, 61);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(125, 111);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn3.Location = new System.Drawing.Point(343, 61);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(125, 111);
+            this.btn3.TabIndex = 1;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn2.Location = new System.Drawing.Point(221, 61);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(125, 111);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn5.Location = new System.Drawing.Point(221, 169);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(125, 111);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn6.Location = new System.Drawing.Point(343, 169);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(125, 111);
+            this.btn6.TabIndex = 4;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn4.Location = new System.Drawing.Point(99, 169);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(125, 111);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn8.Location = new System.Drawing.Point(221, 277);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(125, 111);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn9.Location = new System.Drawing.Point(343, 277);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(125, 111);
+            this.btn9.TabIndex = 7;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btn7.Location = new System.Drawing.Point(99, 277);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(125, 111);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("SimSun", 12F);
+            this.btnReset.Location = new System.Drawing.Point(99, 410);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(369, 63);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("SimSun", 12F);
+            this.lbl1.Location = new System.Drawing.Point(99, 504);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(181, 58);
+            this.lbl1.TabIndex = 10;
+            this.lbl1.Text = "Moves:";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMoves.Font = new System.Drawing.Font("SimSun", 12F);
+            this.lblMoves.Location = new System.Drawing.Point(286, 504);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(182, 58);
+            this.lblMoves.TabIndex = 11;
+            this.lblMoves.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 618);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Blackout Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblMoves;
+    }
+}
+
---
+++ BlackoutGame-10.30.23(ClassLab)/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackoutGame_10._30._23_ClassLab_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //global variable
+        int movesCounters = 0;
+
+        //when game first started, btns are randomized.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GameSetup();
+        }//void Form1_Load
+
+        // when user clicks reset, btns are randomized.
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            GameSetup();
+            EnableBtns();
+
+        }//void btnReset_Click
+
+        ////New Function
+        void swap(ref Button myButton) //orginal needs to be changed, methods can pass any date type of the arguments.
+        {
+            if (myButton.BackColor.Equals(Color.Red))
+            {
+                
+                myButton.BackColor = Color.White;
+                
+            }
+            else
+            {
+                myButton.BackColor = Color.Red;
+            }
+        }//void swap
+
+        ////New Function
+        void GameSetup()
+        {
+
+            Random randomGenrator = new Random();
+            
+
+            //Randomize btn1
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn1.BackColor = Color.Red;
+            }
+            else
+            {
+                btn1.BackColor = Color.White;
+            }
+
+            //Randomize btn2
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn2.BackColor = Color.Red;
+            }
+            else
+            {
+                btn2.BackColor = Color.White;
+            }
+
+            //Randomize btn3
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn3.BackColor = Color.Red;
+            }
+            else
+            {
+                btn3.BackColor = Color.White;
+            }
+
+            //Randomize btn4
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn4.BackColor = Color.Red;
+            }
+            else
+            {
+                btn4.BackColor = Color.White;
+            }
+
+            //Randomize btn5
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn5.BackColor = Color.Red;
+            }
+            else
+            {
+                btn5.BackColor = Color.White;
+            }
+
+            //Randomize btn6
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn6.BackColor = Color.Red;
+            }
+            else
+            {
+                btn6.BackColor = Color.White;
+            }
+
+            //Randomize btn7
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn7.BackColor = Color.Red;
+            }
+            else
+            {
+                btn7.BackColor = Color.White;
+            }
+
+            //Randomize btn8
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn8.BackColor = Color.Red;
+            }
+            else
+            {
+                btn8.BackColor = Color.White;
+            }
+
+            //Randomize btn9
+            if (randomGenrator.Next(0, 2) == 0)
+            {
+                btn9.BackColor = Color.Red;
+            }
+            else
+            {
+                btn9.BackColor = Color.White;
+            }
+
+            movesCounters = 0;
+            lblMoves.Text = movesCounters.ToString();
+
+        }//void GameSetup
+
+        //if game is won. btns need to be disabled
+        private void DisableBtns()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+
+        //if game reset, all btns need to be enabled
+        private void EnableBtns()
+        {
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+        }
+
+        //Check if user wins
+        private void CheckForWin()
+        {
+            //if all background colors are the same, user wins
+            if (btn1.BackColor == btn2.BackColor &&
+                btn1.BackColor == btn3.BackColor &&
+                btn1.BackColor == btn4.BackColor &&
+                btn1.BackColor == btn5.BackColor &&
+                btn1.BackColor == btn6.BackColor &&
+                btn1.BackColor == btn7.BackColor &&
+                btn1.BackColor == btn8.BackColor &&
+                btn1.BackColor == btn9.BackColor)
+            {
+                DisableBtns();
+            }
+        }
+
+        //everytime users clicks a btn, move += 1, then print move, and check if they win
+        private void IncrementMovesNCheckWin()
+        {
+            //moves = moves + 1;
+            movesCounters += 1;
+            lblMoves.Text = movesCounters.ToString();
+
+            //when user clicks each btn, check if they win or not
+            CheckForWin();
+        }
+
+        //when user clicks btn1, what btns swaps
+        ////ANYTHING that is rewritten or reused CAN be written as a method
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //1,2,4
+            swap(ref btn1);
+            swap(ref btn2);
+            swap(ref btn4);
+
+            IncrementMovesNCheckWin();
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //1,2,3,5
+            swap(ref btn1);
+            swap(ref btn2);
+            swap(ref btn3);
+            swap(ref btn5);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            //2,3,6
+            swap(ref btn2);
+            swap(ref btn3);
+            swap(ref btn6);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //1,4,5,7
+            swap(ref btn1);
+            swap(ref btn4);
+            swap(ref btn5);
+            swap(ref btn7);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            //2,4,5,6,8
+            swap(ref btn2);
+            swap(ref btn4);
+            swap(ref btn5);
+            swap(ref btn6);
+            swap(ref btn8);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            //3,5,6,9
+            swap(ref btn3);
+            swap(ref btn5);
+            swap(ref btn6);
+            swap(ref btn9);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            //4,7,8
+            swap(ref btn4);
+            swap(ref btn7);
+            swap(ref btn8);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            //5,7,8,9
+            swap(ref btn5);
+            swap(ref btn7);
+            swap(ref btn8);
+            swap(ref btn9);
+
+            IncrementMovesNCheckWin();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            //6,8,9
+            swap(ref btn6);
+            swap(ref btn8);
+            swap(ref btn9);
+
+            IncrementMovesNCheckWin();
+        }
+    }
+}
